f8299d1ae52d86a71d7630432170fcbf17885717
DanielRGilles <danryangilles@gmail.com>
2021-09-21 14:54:32 -0700

    added working creature filter, working on horns filter

--- src/App.js
+++ src/App.js
@@ -4,37 +4,43 @@ import './App.css';
 import Header from './components/Header.js';
 
 export default class App extends Component {
-  
-  ShowCreatures = () => {
-   const creatureEl = document.getElementById('horned')
-   const choiceEl = creatureEl.options[creatureEl.selectedIndex].value
-   
-   {creatures.filter(creature => creature.keyword === choiceEl).map(creature => {
-     return (
-   <li className='creature' key={creature.url}>
-       {creature.title}  <br></br> 
-       <img src={creature.url} alt='horned creature'/><br></br>
-       {creature.description}
-   </li>
-
-      )})
-  
+  state = {
+    keyword: '',
+    horns: ''
   }
-
-  render() 
-  {
+  handleChange = (e) => {
+    this.setState({ keyword: e.target.value })
+}
+  handleHornChange = (e) => {
+  this.setState({ horns: e.target.value })
+}
+  render() {
+    const selectedCreatures = creatures
+    .filter(creature => {
+      if (!this.state.keyword){
+    return true }
+     return (creature.keyword === this.state.keyword) });
+     
+     const moreFiltered =
+     selectedCreatures
+     .filter(creature => {
+      if (!this.state.horns){
+    return true }
+     return (Number(creature.horns) === this.state.horns) })
+     
     return (
       <div>
         <Header/>
-        <h1>Choose Image type</h1>
-        <label>Choose a Horned Creature:</label>
-       <select id='horned' name='horned'> 
+        <div className='creature-select'>
+        
+        <label>Choose a Horned Creature  </label>
+       <select onChange={this.handleChange}> 
         <option value=''></option>
         <option value='narwhal'>Narwhal</option>
         <option value='rhino'>Rhino</option>
         <option value='unicorn'>Unicorn</option>
         <option value='unilego'>Unilego</option>
-        <option value='tricerotops'>Tricerotops</option>
+        <option value='triceratops'>Triceratops</option>
         <option value='markhor'>Markhor</option>
         <option value='mouflon'>Mouflan</option>
         <option value='addax'>Addax</option>
@@ -42,10 +48,26 @@ export default class App extends Component {
         <option value='lizard'>Lizard</option>
         <option value='dragon'>Dragon</option>
         </select>
-        <button onClick={this.ShowCreatures}>Show Creatures</button>
+        <label>  Number of horns  </label>
+        <select onChange={this.handleHornChange}> 
+        <option value=''>All</option>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        </select>
+        </div>
         <ul className='list'>
-          
+           {moreFiltered.map(creature => {
+            return (
+          <li className='creature' key={creature.url}>
+              <div className='creature-neem'>{creature.title}</div>  <br></br> 
+              <img src={creature.url} alt='horned creature'/><br></br>
+              <div className='creature-deets'>{creature.description}</div>
+          </li>
+
+             )})}
         </ul>
       </div>
     )
-  }}
+  }
+}
