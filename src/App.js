0ff385ec1accccd907238d409c2e6ca4ff5c12a5
DanielRGilles <danryangilles@gmail.com>
2021-09-21 13:38:54 -0700

    added rendering of data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react'
-import images from './data.js'
+import creatures from './data.js'
 import './App.css';
+import Header from './components/Header.js';
 
 export default class App extends Component {
-  state = {
+  
+  ShowCreatures = () => {
+   const creatureEl = document.getElementById('horned')
+   const choiceEl = creatureEl.options[creatureEl.selectedIndex].value
+   
+   {creatures.filter(creature => creature.keyword === choiceEl).map(creature => {
+     return (
+   <li className='creature' key={creature.url}>
+       {creature.title}  <br></br> 
+       <img src={creature.url} alt='horned creature'/><br></br>
+       {creature.description}
+   </li>
 
+      )})
+  
   }
-  render() {
+
+  render() 
+  {
     return (
       <div>
+        <Header/>
         <h1>Choose Image type</h1>
-        <label for="horned">Choose a Horned Creature:</label>
+        <label>Choose a Horned Creature:</label>
        <select id='horned' name='horned'> 
         <option value=''></option>
         <option value='narwhal'>Narwhal</option>
@@ -25,8 +42,10 @@ export default class App extends Component {
         <option value='lizard'>Lizard</option>
         <option value='dragon'>Dragon</option>
         </select>
-        <button onClick={}>Show Creatures</button>
+        <button onClick={this.ShowCreatures}>Show Creatures</button>
+        <ul className='list'>
+          
+        </ul>
       </div>
     )
-  }
-}
+  }}
